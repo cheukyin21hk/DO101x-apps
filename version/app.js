a7d03c1164fbb2a51f33ebb750541866218b22e0
Chris_Ng_355 <cheukyin21hk@gmail.com>
2025-02-28 00:15:37 +0800

    version 3

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 2 of the app.' + '\n';
+    response = 'This is version 3 of the app.' + '\n';
 
     //send the response to the client
     res.send(response);
@@ -15,3 +15,7 @@ app.get('/', function (req, res) {
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
 });
+
+oc delete pods version-5446bcc944-wmp9b 
+oc delete pods version-5446bcc944-zdsjn
+oc delete pods version-5446bcc944-zqxtv 
